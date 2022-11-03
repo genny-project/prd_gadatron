0b2a4781361de804734196dc3e8b3d6b1b183cc0
Adam Crow <adamcrow63@gmail.com>
2022-11-03 17:11:47 +1100

    Updated route to handle test question route

--- scripts/test_questions.sh
+++ scripts/test_questions.sh
@@ -5,7 +5,7 @@ targetCode=${3:-PER_BF0E997C-4AE0-46C1-B485-E46B077DF5C8}
 sourceCode=PER_0F6169E1-FDD5-4DAF-BEC3-4126C6626752
 TOKEN=`./gettoken-cache.sh ${productCode}`
 echo $TOKEN
-payload="{\"data\":{\"sourceCode\":\"${sourceCode}\",\"targetCode\":\"${targetCode}\",\"code\":\"TEST_${questionCode}\"},\"token\":\"${TOKEN}\",\"msg_type\":\"EVT_MSG\"}"
+payload="{\"data\":{\"sourceCode\":\"${sourceCode}\",\"targetCode\":\"${targetCode}\",\"code\":\"TESTQ_${questionCode}\"},\"token\":\"${TOKEN}\",\"msg_type\":\"EVT_MSG\"}"
 echo $payload
 echo  $payload > event.json
 
