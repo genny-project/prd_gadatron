5d2b53693458c167349691d23a8c1b1ca6e5ef60
Adam Crow <adamcrow63@gmail.com>
2022-11-18 11:38:18 +0800

    Updated to work

--- scripts/gettoken-prod.sh
+++ scripts/gettoken-prod.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 set -e
 realm=internmatch
-password=`echo $USER_PASSWORD`
+realm=`echo $GENNY_KEYCLOAK_REALM`
+password=`echo $GENNY_SERVICE_PASSWORD`
 clientid=`echo $GENNY_CLIENT_ID`
 secret=`echo $GENNY_CLIENT_SECRET`
-
-KEYCLOAK_RESPONSE=`curl -s -X POST https://keycloak.gada.io/auth/realms/${realm}/protocol/openid-connect/token  -H "Content-Type: application/x-www-form-urlencoded" -d 'username=testuser@gada.io' -d 'password='$password'' -d 'grant_type=password' -d 'client_id='$clientid''  -d 'client_secret='$secret''`
+keycloakurl=`echo $GENNY_KEYCLOAK_URL`
+username=`echo $GENNY_SERVICE_USERNAME`
+#echo "realm=$realm"
+#echo "keycloakurl=$keycloakurl"
+#echo "clientid=$clientid"
+#echo "secret=$secret"
+#echo "username=$username"
+#echo "password=$password"
+KEYCLOAK_RESPONSE=`curl -s -X POST ${keycloakurl}/realms/${realm}/protocol/openid-connect/token  -H "Content-Type: application/x-www-form-urlencoded" -d 'username='$username'' -d 'password='$password'' -d 'grant_type=password' -d 'client_id='$clientid''  -d 'client_secret='$secret''`
 
 ACCESS_TOKEN=`echo "$KEYCLOAK_RESPONSE" | jq -r '.access_token'`
 echo ${ACCESS_TOKEN}  
