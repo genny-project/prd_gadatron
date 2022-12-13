43117c1fc101574fc57874bab8b25e9bc422571a
wayanMastra <wayan.mastra@gada.io>
2022-12-13 14:34:15 +0800

    saving changes
    Signed-off-by: wayanMastra <wayan.mastra@gada.io>

--- src/main/java/life/genny/gadatron/route/Events.java
+++ src/main/java/life/genny/gadatron/route/Events.java
@@ -1,8 +1,6 @@
 package life.genny.gadatron.route;
 
-import life.genny.gadatron.service.AmrizalService;
-import life.genny.gadatron.service.BalService;
-import life.genny.gadatron.service.GarService;
+import life.genny.gadatron.service.WayanService;
 import life.genny.kogito.common.utils.KogitoUtils;
 import life.genny.qwandaq.Answer;
 import life.genny.qwandaq.attribute.Attribute;
@@ -32,8 +30,6 @@ public class Events {
 
 	static final Logger log = Logger.getLogger(MethodHandles.lookup().lookupClass());
 
-	private static final String DEF_BALI = "DEF_BALI_PERSON";
-
 	@Inject
 	UserToken userToken;
 
@@ -47,13 +43,7 @@ public class Events {
 	BaseEntityUtils beUtils;
 
 	@Inject
-	BalService balService;
-
-	@Inject
-	AmrizalService amService;
-
-	@Inject
-	GarService garService;
+	WayanService wayanService;
 
 	/**
 	 * @param msg
@@ -185,167 +175,6 @@ public class Events {
 			}
 		}
 
-		// test question event
-		if (code.startsWith("UNUSED_GADA_TAZ_CREATE_PER")) {
-			log.info("Creating Taz Person ..." + msg.getData().getCode() + " msg=" + msg);
-
-			// BUILD JSON PAYLOAD CONTENT
-			JsonObjectBuilder payloadBuilder = Json.createObjectBuilder()
-					.add("questionCode", msg.getData().getCode().substring("GADA_TAZ_CREATE_PER".length()))
-					.add("entityCode", msg.getData().getTargetCode())
-					.add("targetCode", msg.getData().getTargetCode());
-
-			String content = msg.getData().getContent();
-			if (content != null) {
-				payloadBuilder.add("content", content);
-
-				log.info("Content = " + content);
-			}
-
-			if (userToken != null) {
-				if (userToken.getUserCode() != null) {
-					payloadBuilder.add("sourceCode", userToken.getUserCode());
-					payloadBuilder.add("userCode", userToken.getUserCode());
-				}
-			}
-
-			JsonObject payload = payloadBuilder.build();
-			System.out.println("Payload = " + payload.toString());
-//			kogitoUtils.triggerWorkflow(SELF, "testQuestionGT2", payload);
-
-			log.info("Payload = " + payload);
-
-			// kogitoUtils.triggerWorkflow(SELF, "testQuestionGT2", payload);
-			balService.createPersonBal("DEF_PERSON", content);
-			return;
-		}
-
-		if (code.startsWith("GADA_TAZ_")) {
-			// BPMN_TST
-			log.info("GADA_TAZ Event Listener ..." + msg.getData().getCode() + " msg=" + msg);
-
-			JsonObjectBuilder payloadBuilder = Json.createObjectBuilder()
-				.add("questionCode", msg.getData().getCode().substring("GADA_TAZ_".length()))
-				.add("entityCode", msg.getData().getTargetCode())
-				.add("targetCode", msg.getData().getTargetCode());
-
-			// GET STRING CONTENT FROM PAYLOAD
-			String content = msg.getData().getContent();
-			log.debug(content);
-
-			if (content != null) {
-				payloadBuilder.add("content", content);
-				// System.out.println("Content = " + content);
-				log.info("Content = " + content);
-			}
-			// CHECK FOR TOKEN EVENT
-			if (userToken != null) {
-				if (userToken.getUserCode() != null) {
-					payloadBuilder.add("sourceCode", userToken.getUserCode());
-					payloadBuilder.add("userCode", userToken.getUserCode());
-				}
-			} else {
-				log.error("userToken is Null");
-			}
-
-			JsonObject payload = payloadBuilder.build();
-			log.info("Payload = " + payload.toString());
-
-			// CHECK QUESTION
-			if (code.endsWith("CREATE_PER")) {
-				balService.createPersonBal("DEF_PERSON", content);
-			} else if (code.endsWith("CREATE_PER_BPMN")) {
-				kogitoUtils.triggerWorkflow(SELF, "testBal", payload);
-			} else {
-				kogitoUtils.triggerWorkflow(SELF, "testBal", payload);
-				// log.error("Invalid Question Code: " + msg.getData().getCode().substring("GADA_TAZ_".length()));
-			}
-		}
-
-		// test question eventIrvan
-		if (code.startsWith("GADA_IRVAN_")) {
-			log.info("Irvan Listener ..." + msg.getData().getCode() + " msg=" + msg);
-			JsonObjectBuilder payloadBuilder = Json.createObjectBuilder()
-					.add("questionCode", msg.getData().getCode().substring("GADA_IRVAN_".length()))
-					.add("entityCode", msg.getData().getTargetCode())
-					.add("targetCode", msg.getData().getTargetCode());
-
-			String content = msg.getData().getContent();
-			if (content != null) {
-				payloadBuilder.add("content", content);
-
-				log.info("Content = " + content);
-				/* Load the LNK_DOT */
-				if (content.startsWith("DOT_")) {
-					BaseEntity target = beUtils.getBaseEntityByCode(PRODUCT_CODE, msg.getData().getTargetCode());
-					Attribute lnkDot = qwandaUtils.getAttribute("LNK_DOT");
-					target.addAnswer(new Answer(target, target, lnkDot, "[\"" + content + "\"]"));
-					beUtils.updateBaseEntity(PRODUCT_CODE, target);
-				}
-
-			}
-			if (userToken != null) {
-				if (userToken.getUserCode() != null) {
-					payloadBuilder.add("sourceCode", userToken.getUserCode());
-					payloadBuilder.add("userCode", userToken.getUserCode());
-				}
-			}
-
-			JsonObject payload = payloadBuilder.build();
-
-			log.info("Payload = " + payload.toString());
-
-			kogitoUtils.triggerWorkflow(SELF, "testQuestionIrvan", payload);
-			return;
-		}
-
-		if (code.startsWith("GADA_AMRIZAL_QUE_")) {
-			log.info("Displaying Amrizal test question ..." + msg.getData().getCode() + " msg=" + msg);
-			JsonObjectBuilder payloadBuilder = Json.createObjectBuilder()
-					.add("questionCode", msg.getData().getCode().substring("GADA_AMRIZAL_".length()));
-
-			String content = msg.getData().getContent();
-			if (content != null) {
-				payloadBuilder.add("content", content);
-
-				log.info("Content = " + content);
-				/* Load the LNK_DOT */
-
-				String amrizalCode = amService.createPersonAmrizal(DEF_BALI, content);
-
-				if (amrizalCode != null)
-					payloadBuilder.add("entityCode", amrizalCode).add("targetCode", amrizalCode);
-				else
-					payloadBuilder
-							.add("entityCode", msg.getData().getTargetCode())
-							.add("targetCode", msg.getData().getTargetCode());
-
-				BaseEntity target = beUtils.getBaseEntityByCode(PRODUCT_CODE, amrizalCode);
-				Attribute lnkDot = qwandaUtils.getAttribute("LNK_DOT");
-				target.addAnswer(new Answer(target, target, lnkDot, "[\"" + content + "\"]"));
-				beUtils.updateBaseEntity(PRODUCT_CODE, target);
-			}
-
-			if (userToken != null) {
-				if (userToken.getUserCode() != null) {
-					payloadBuilder.add("sourceCode", userToken.getUserCode());
-					payloadBuilder.add("userCode", userToken.getUserCode());
-				}
-			} else {
-				log.error("userToken is Null");
-			}
-
-			JsonObject payload = payloadBuilder.build();
-
-			log.info("Payload = " + payload.toString());
-
-			if (kogitoUtils != null) {
-				kogitoUtils.triggerWorkflow(SELF, "testQuestionGT2", payload);
-			} else {
-				log.error("kogitoUtils is Null");
-			}
-		}
-
 		if (code.startsWith("QUE_WAYAN_") || code.equals("QUE_USER_PROFILE_VIEW")) {
 			String sourceCode = msg.getData().getSourceCode();
 			if (sourceCode == null || sourceCode.isEmpty()) return;
@@ -377,43 +206,6 @@ public class Events {
 
 			JsonObject payload = payloadBuilder.build();
 			kogitoUtils.triggerWorkflow(SELF, "testWayanQuestion", payload);
-			return;
-		}
-
-		if (code.startsWith("GADA_GAR_CREATE_PER_")) {
-			//String entityCode = garService.createPerson("DEF_PERSON",  data.getContent());
-			//log.info("Create entityCode: " + entityCode);
-			JsonObjectBuilder payloadBuilder = Json.createObjectBuilder()
-				.add("questionCode", msg.getData().getCode().substring("GADA_GAR_CREATE_PER_".length()))
-				.add("userCode", userToken.getUserCode())
-				.add("sourceCode", userToken.getUserCode())
-				//.add("entityCode", entityCode)
-				.add("targetCode", msg.getData().getTargetCode());
-
-			String content = data.getContent();
-			if (content != null) {
-				payloadBuilder.add("content", content);
-			}
-
-			JsonObject payload = payloadBuilder.build();
-			System.out.println("Payload = " + payload.toString());
-
-			kogitoUtils.triggerWorkflow(SELF, "testPersonGardiary", payload);
-			return;
-		}
-
-		if (code.equals("QUE_TEST_GARDIARY")) {
-			log.info("Entering " + code);
-			JsonObjectBuilder payloadBuilder = Json.createObjectBuilder()
-				.add("questionCode", "QUE_TEST_GARDIARY_GRP")
-				.add("userCode", userToken.getUserCode())
-				.add("sourceCode", userToken.getUserCode())
-				.add("targetCode", GarService.personEntityCode);
-
-			JsonObject payload = payloadBuilder.build();
-			System.out.println("Payload = " + payload.toString());
-
-			kogitoUtils.triggerWorkflow(SELF, "testQuestionGardiary", payload);
 			return;
 		}
 	}
