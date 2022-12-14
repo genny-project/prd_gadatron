b592d8dee4048bc56549702d50cd8a2ca17aae6b
wayanMastra <wayan.mastra@gada.io>
2022-12-14 15:29:09 +0800

    pull some code from 10.3.0

--- src/main/java/life/genny/gadatron/cache/SearchCaching.java
+++ src/main/java/life/genny/gadatron/cache/SearchCaching.java
@@ -1,28 +1,22 @@
 package life.genny.gadatron.cache;
 
-import static life.genny.gadatron.constants.GadatronConstants.PRODUCT_CODE;
+import life.genny.gadatron.constants.GadatronConstants;
+import life.genny.qwandaq.EEntityStatus;
+import life.genny.qwandaq.attribute.Attribute;
+import life.genny.qwandaq.datatype.DataType;
+import life.genny.qwandaq.entity.search.SearchEntity;
+import life.genny.qwandaq.entity.search.trait.*;
+import life.genny.qwandaq.models.GennySettings;
+import life.genny.qwandaq.utils.CacheUtils;
+import org.jboss.logging.Logger;
 
+import javax.enterprise.context.ApplicationScoped;
 import java.lang.invoke.MethodHandles;
 import java.util.ArrayList;
 import java.util.List;
 
-import life.genny.qwandaq.EEntityStatus;
-import life.genny.qwandaq.entity.search.SearchEntity;
-import life.genny.qwandaq.attribute.Attribute;
-import life.genny.qwandaq.datatype.DataType;
-import life.genny.qwandaq.models.GennySettings;
-import org.jboss.logging.Logger;
-
-import life.genny.qwandaq.entity.search.trait.Action;
-import life.genny.qwandaq.entity.search.trait.Column;
-import life.genny.qwandaq.entity.search.trait.AssociatedColumn;
-import life.genny.qwandaq.entity.search.trait.Filter;
-import life.genny.qwandaq.entity.search.trait.Operator;
-import life.genny.qwandaq.entity.search.trait.Ord;
-import life.genny.qwandaq.entity.search.trait.Sort;
-import life.genny.qwandaq.utils.CacheUtils;
-
-import javax.enterprise.context.ApplicationScoped;
+import static life.genny.gadatron.constants.GadatronConstants.PRODUCT_CODE;
+import static life.genny.qwandaq.attribute.Attribute.LNK_DEF;
 
 @ApplicationScoped
 public class SearchCaching {
@@ -53,6 +47,8 @@ public class SearchCaching {
 	public static final String SBE_TABLE_HOST_COMPANIES = "SBE_TABLE_HOST_COMPANIES";
 	public static final String SBE_TABLE_INTERNSHIPS = "SBE_TABLE_INTERNSHIPS";
 	public static final String SBE_TABLE_LOGBOOK = "SBE_TABLE_LOGBOOK";
+	public static final String SBE_TABLE_PERSON = "SBE_TABLE_PERSON";
+	public static final String SBE_TABLE_BALI_PERSON = "SBE_TABLE_BALI_PERSON";
 
 	// contacts
 	public static final String SBE_TABLE_AGENTS = "SBE_TABLE_AGENTS";
@@ -70,6 +66,12 @@ public class SearchCaching {
 	public static final String SBE_OFFERED_APPLICATIONS = "SBE_OFFERED_APPLICATIONS";
 	public static final String SBE_PLACED_APPLICATIONS = "SBE_PLACED_APPLICATIONS";
 	public static final String SBE_SHORTLISTED_APPLICATIONS = "SBE_SHORTLISTED_APPLICATIONS";
+
+	public static final String VIEW = "VIEW";
+	public static final String EDIT = "EDIT";
+	public static final String APPLY = "APPLY";
+	public static final String CREATE = "CREATE";
+	public static final String DELETE = "DELETE";
 
 	public void saveToCache(String realm) {
 		log.info("=========================saveToCache=========================");
@@ -205,6 +207,28 @@ public class SearchCaching {
 				.add(new Action("PRI_EVENT_VIEW", "View Profile"))
 				.setPageStart(0).setPageSize(GennySettings.defaultBucketSize()));
 
+		cacheSearch(
+				new SearchEntity(SBE_TABLE_PERSON, "People")
+						.add(new Filter(LNK_DEF, Operator.CONTAINS, GadatronConstants.DEF_PERSON))
+						// View own application only
+						// .add(Trait.decorator(new Filter(LNK_PERSON, Operator.CONTAINS, "USER_CODE"))
+						// .addCapabilityRequirement(
+						// new CapabilityBuilder(PERSON).view(PermissionMode.SELF)
+						// .view(PermissionMode.ALL, true)
+						// .buildCap())
+						// .build())
+						// .add(new AssociatedColumn(LNK_STAFF, PRI_POSITION, "Staff Position"))
+						.add(new Column("PRI_IMAGE_URL", "Profile Picture"))
+						.add(new Column("PRI_NAME", "Name"))
+						.add(new Column("PRI_CREATED", "Date Created"))
+						.add(new Column("PRI_STATUS", "Status"))
+						.add(new AssociatedColumn("LNK_COMPANY", "PRI_NAME", "Company"))
+						.add(new Column("PRI_EMAIL", "Email"))
+						.add(new Column("PRI_MOBILE", "Mobile"))
+						.add(new Action(VIEW, "View"))
+						.setPageSize(20) // TODO, use default page size
+						.setPageStart(0));
+
 		List<SearchEntity> entities = new ArrayList<>();
 
 		// sidebar
@@ -251,7 +275,6 @@ public class SearchCaching {
 		cacheDropdown("DEF_COMPANY",
 				new SearchEntity("SBE_SER_LNK_NUMBER_STAFF", "Number of Staffs Dropdown")
 						.setLinkValue("NO_OF_STAFF"));
-
 
 		// DEF_HOST_CPY_REP
 		cacheDropdown("DEF_HOST_CPY_REP",
@@ -303,7 +326,6 @@ public class SearchCaching {
 				new SearchEntity("SBE_SER_LNK_SOFTWARE", "Software Dropdown")
 						.setLinkValue("SOFTWARE"));
 
-
 		cacheDropdown("DEF_APPLICATION",
 				new SearchEntity("SBE_SER_LNK_HOST_COMPANY", "Host Company Dropdown")
 						.add(new Filter("PRI_IS_HOST_CPY", true)));
@@ -312,6 +334,9 @@ public class SearchCaching {
 						.add(new Filter("PRI_IS_INTERNSHIP", true))
 						.add(new Filter("LNK_HOST_COMPANY", Operator.CONTAINS, "[[SOURCE.LNK_HOST_COMPANY]]")));
 
+		cacheDropdown("DEF_BALI_PERSON",
+				new SearchEntity("SBE_SER_LNK_LANGUAGES", "Languages Dropdown")
+						.setLinkValue("LANGUAGES"));
 	}
 
 	/**
@@ -602,7 +627,7 @@ public class SearchCaching {
 				/* Filterable Columns */
 				.addFilterableColumn("TEXT_PRI_NAME", "Name")
 				.addFilterableColumn("YES_NO_PRI_DJP_AGREE", "DJP Agree")
-//				.addFilterableColumn("PRI_TEXT_INDUSTRY", "Company Industry")
+				// .addFilterableColumn("PRI_TEXT_INDUSTRY", "Company Industry")
 				.addFilterableColumn("TEXT_PRI_VALIDATION", "Validation")
 				.addFilterableColumn("TEXT_PRI_STATUS", "Status")
 				.addFilterableColumn("DATETIME_PRI_CREATED_DATE", "Date Created")
@@ -668,7 +693,7 @@ public class SearchCaching {
 				.addFilterableColumn("TEXT_PRI_ASSOC_HC", "Host Company")
 				.addFilterableColumn("SELECT_PRI_COUNTRY_FIELD_PRI_ADDRESS_COUNTRY", "Country")
 				.addFilterableColumn("DATETIME_PRI_CREATED", "Date Created")
-//				.addFilterableColumn("SELECT_PRI_INTERNSHIP_TYPE", "Internship Type")
+				// .addFilterableColumn("SELECT_PRI_INTERNSHIP_TYPE", "Internship Type")
 
 				/* Wildcard blacklist */
 				/* .addBlacklist("PRI_ROLES_AND_RESPONSIBILITIES") */
--- src/main/java/life/genny/gadatron/constants/GadatronConstants.java
+++ src/main/java/life/genny/gadatron/constants/GadatronConstants.java
@@ -6,12 +6,27 @@ public class GadatronConstants {
 
 	// product
 	public static final String PRODUCT_CODE = "gadatron";
+
 	// definitions
+	public static final String DEF_BALI_PERSON = "DEF_BALI_PERSON";
+	public static final String DEF_PERSON = "DEF_PERSON";
+	public static final String DEF_APPLICATION = "DEF_APPLICATION";
+	public static final String DEF_COMPANY = "DEF_COMPANY";
 
-	
-// capabilities
+	// capabilities
 	public static final String ADMIN = "ADMIN";
+	public static final String BALI_PERSON = "BALI_PERSON";
+	public static final String PERSON = "PERSON";
+	public static final String COMPANY = "COMPANY";
 
-	// rolesV
+	// roles
 	public static final String ADMIN_ROLE = Prefix.ROL.concat(ADMIN);
+	public static final String BALI_PERSON_ROLE = Prefix.ROL.concat(BALI_PERSON);
+
+	public static final String QUE_TABLE_PERSON = "QUE_TABLE_PERSON";
+	public static final String QUE_TABLE_COMPANY = "QUE_TABLE_COMPANY";
+
+	public static final String DASHBOARD = "DASHBOARD_VIEW";
+
+	public static final String ACTION_APPLY = "ACTION_APPLY";
 }
