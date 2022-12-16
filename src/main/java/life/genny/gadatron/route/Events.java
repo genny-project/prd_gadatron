c0ed45f62abc79b219ba25d409e3ad617e9d3a2a
wayanMastra <wayan.mastra@gada.io>
2022-12-16 12:30:36 +0800

    add a question handler to Event.java

--- src/main/java/life/genny/gadatron/route/Events.java
+++ src/main/java/life/genny/gadatron/route/Events.java
@@ -156,6 +156,7 @@ public class Events {
 			} else {
 				log.error("kogitoUtils is Null");
 			}
+			return;
 		}
 
 		if (code.startsWith("GADA_WAYAN_")) {
@@ -183,6 +184,7 @@ public class Events {
 					kogitoUtils.triggerWorkflow(SELF, "testWayan", payload);
 				}
 			}
+			return;
 		}
 
 		// test question event
@@ -260,6 +262,7 @@ public class Events {
 				kogitoUtils.triggerWorkflow(SELF, "testBal", payload);
 				// log.error("Invalid Question Code: " + msg.getData().getCode().substring("GADA_TAZ_".length()));
 			}
+			return;
 		}
 
 		// test question eventIrvan
@@ -344,20 +347,23 @@ public class Events {
 			} else {
 				log.error("kogitoUtils is Null");
 			}
+			return;
 		}
 
-		if (code.startsWith("QUE_WAYAN_")) {
+		if (code.startsWith("QUE_WAYAN_") || code.equals("QUE_USER_PROFILE_VIEW")) {
 			String sourceCode = msg.getData().getSourceCode();
 			if (sourceCode == null || sourceCode.isEmpty()) return;
 			String nextQuestionCode = switch (code) {
 				case "QUE_WAYAN_CREATEQUESTION_VIEW" -> "QUE_WAYAN_CREATEQUESTION_GRP";
 				case "QUE_WAYAN_CREATEBALIPERSON_VIEW" -> "QUE_BALI_PERSON_GRP";
+				case "QUE_USER_PROFILE_VIEW" -> "QUE_USER_PROFILE_GRP";
 				default -> "";
 			};
 
 			String entityCode = switch (code) {
 				case "QUE_WAYAN_CREATEQUESTION_VIEW" -> "DEF_QUESTION";
 				case "QUE_WAYAN_CREATEBALIPERSON_VIEW" -> "DEF_BALI_PERSON";
+				case "QUE_USER_PROFILE_VIEW" -> "DEF_PERSON";
 				default -> "";
 			};
 
